c32462986194dc4556f538f4adfb5bd6a7536126
João Pedro Alves <jotalmeida007@hotmail.com>
2021-02-24 01:17:47 -0300

    Add function to deposit values in account

--- lib/rocketpay/account.ex
+++ lib/rocketpay/account.ex
@@ -16,7 +16,7 @@ defmodule Rocketpay.Account do
     timestamps()
   end
 
-  def changeset(params) do
+  def changeset(struct \\ %__MODULE__{}, params) do
     %__MODULE__{}
     |> cast(params, @required_params)
     |> validate_required(@required_params)
